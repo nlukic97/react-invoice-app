3b3b5573836dac3c8636fd60895f4e137bf94fc3
nlukic97 <nlukic97@gmail.com>
2021-11-16 16:12:42 +0100

    comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './css/App.css';
+import {useState, useEffect} from 'react';
 
+// data
+import Data from './data/data.json';
+
+// components
 import Sidebar from './components/Sidebar';
 
 function App() {
 
+  // data imported from the json file
+  const [data, setData] = useState(Data)
   
+  //upon having the data rendered, log it to the console
+  useEffect(function(){
+    console.log(data);
+  },[data])
+
 
   return (
     <div>
-      <h1>Cao</h1>
       <Sidebar />
+      <h1>Cao</h1>
     </div>
   );
 }
