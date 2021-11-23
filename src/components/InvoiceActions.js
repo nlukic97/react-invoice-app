23e07c96f6048a600a9b67d71835b206c396af54
nlukic97 <nlukic97@gmail.com>
2021-11-23 12:10:45 +0100

    different events for displaying the filter options (comment added for possible mobile solution

--- src/components/InvoiceActions.js
+++ src/components/InvoiceActions.js
@@ -21,6 +21,7 @@ const InvoiceActions = ({message, changedFilter, checkedBoxes}) => {
                 <div className="___filter_container" 
                     onMouseEnter={()=> setIsHover(true)} 
                     onMouseLeave={()=>setIsHover(false)}
+                    // onClick={()=>setIsHover(!isHover)} //this to be used for render in different screen
                 >
                     <h4>Filter by status</h4>
                     <svg width="11" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4.228 4.228L9.456 1" stroke="#7C5DFA" strokeWidth="2" fill="none" fillRule="evenodd"/></svg>
