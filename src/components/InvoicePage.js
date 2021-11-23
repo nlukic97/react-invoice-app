2f6e988222b25993b4f241f6176994cfe7639227
nlukic97 <nlukic97@gmail.com>
2021-11-23 14:56:02 +0100

    sorted out styling for the desktop version of the single page, working on displaying the data

--- src/components/InvoicePage.js
+++ src/components/InvoicePage.js
@@ -2,28 +2,31 @@ import { Link, useNavigate } from "react-router-dom";
 import { useParams } from "react-router"
 
 const InvoicePage = ({invoices, deleteInvoice, markAsPaid}) => {
-    let navigate = useNavigate()
 
+    let navigate = useNavigate()
     let {id} = useParams()
     let invoice = invoices.find(item=> item.id === id)
+
     console.log(invoice);
+
     return (
-        <div>
+        <div className="single_page_invoice">
             <Link to="/">Back</Link>
             {(!invoice) ? <h1>This invoice does not exist</h1> : <h1>Hello, this is the invoice page {invoice.id}</h1>}
 
-            <div className="card">
-                {/* Status container */}
-                <div className="status">
-                    <span>status </span>
+            <div className="flex-container">
+                
+                {/* 1. Status container */}
+                <div className="status-container card">
+                    <span className="status_label body-1">Status</span>
                     <div className={`__invoice_status ${invoice.status}`}>
                         <div className='__invoice_status_circle'></div>
                         <span>{invoice.status}</span>
                     </div>
                 </div>
 
-                {/* Button container */}
-                <div className="button-container">
+                {/* 2 Button container */}
+                <div className="button-container card">
                     <button className="btn gray">
                         <h4>Edit</h4>
                     </button>
@@ -36,12 +39,32 @@ const InvoicePage = ({invoices, deleteInvoice, markAsPaid}) => {
                         <h4>Delete</h4>
                     </button>
 
-                    <button className="btn purple" onClick={()=>{markAsPaid(invoice.id)}}>
-                        <h4>Mark as Paid</h4>
-                    </button>
+                    {invoice.status !== 'paid' ? (
+                        <button className="btn purple" onClick={()=>{markAsPaid(invoice.id)}}>
+                            <h4>Mark as Paid</h4>
+                        </button>)
+                    :
+                        (null)
+                    }
+                </div>
+                
+                {/* 3. Invoice data */}
+                <div className="data-container card">
+                    
+                    <div className="id_address_container">
+                        <div className="id_description_container">
+                            <span className="id">#{invoice.id}</span>
+                            <span className="description">{invoice.description}</span>
+                        </div>
+                    </div>
+                    <div className="sender_address_container">
+                        <span className="street">{invoice.senderAddress.street}</span>
+                        <span className="city">{invoice.senderAddress.city}</span>
+                        <span className="postCode">{invoice.senderAddress.postCode}</span>
+                        <span className="country">{invoice.senderAddress.country}</span>
+                    </div>
                 </div>
             </div>
-
         </div>
     )
 }
