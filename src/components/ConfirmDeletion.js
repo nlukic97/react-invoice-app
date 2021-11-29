6bef9fb114815a1f4c7e4c4156effdc933b761fc
nlukic97 <nlukic97@gmail.com>
2021-11-29 16:40:41 +0100

    styled for mobile, and also created the delete confirmation modal

--- src/components/ConfirmDeletion.js
+++ src/components/ConfirmDeletion.js
@@ -6,7 +6,7 @@ const ConfirmDeletion = ({deleteConfirmation}) => {
             <div className="confirm-delete">
                 <h1>Confirm Deletion</h1>
                 <span className="confirm-deletion-body">Are you sure you want to delete invoice #XM9141? This action cannot be undone.</span>
-                <div>
+                <div className="buttons-container">
                     <button className="btn gray" onClick={()=> deleteConfirmation(false)}>Cancel</button>
                     <button className="btn red" onClick={()=> deleteConfirmation(true)}>Delete</button>
 
